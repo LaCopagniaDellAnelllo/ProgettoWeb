f9067b6ae1cc9620d0bfec8f818e6f70337025e7
federipoli <federico.ripoli@hotmail.it>
2018-06-28 11:10:02 +0200

    servlet1

--- Progetto web/src/control/Accesso.java
+++ Progetto web/src/control/Accesso.java
@@ -5,10 +5,13 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class Accesso extends HttpServlet{
 
+public class Accesso extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+
+	
 	public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		
+		doPost(request,response);
 	}
 	
 	public void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
